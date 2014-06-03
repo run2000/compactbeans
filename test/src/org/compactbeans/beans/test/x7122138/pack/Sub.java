5c1731e4fda001732da12be3ea5b35771bb10e27
Nicholas Cull <run2000@gmail.com>
2014-06-03 22:26:02 +1000

    Added tests from OpenJDK. These are GPL2 only, not part of the distribution.

---
+++ test/src/org/compactbeans/beans/test/x7122138/pack/Sub.java
@@ -0,0 +1,4 @@
+package org.compactbeans.beans.test.x7122138.pack;
+
+public class Sub<String> extends Super {
+}
---
+++ test/src/org/compactbeans/beans/test/x7122138/pack/Super.java
@@ -0,0 +1,14 @@
+package org.compactbeans.beans.test.x7122138.pack;
+
+class Super<T> {
+
+    T name;
+
+    public void setName(T name) {
+        this.name = name;
+    }
+
+    public T getName() {
+        return name;
+    }
+}
